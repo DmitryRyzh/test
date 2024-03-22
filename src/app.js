f1981d2dbdc277bff7cd1d4fdd5013cf03df5036
DmitryRyzh <epiccraft556@mail.ru>
2024-03-22 07:55:38 +0500

    почти готово

--- src/app.js
+++ src/app.js
@@ -17,42 +17,76 @@ var HelloWorldLayer = cc.Layer.extend({
     ctor:function () {
         this._super();
         
-        var background = new cc.Sprite(res.background_jpg);
+        var background = new cc.Sprite(res.background_jpg); // фон
         background.attr({
             x: cc.winSize.width/2,
             y: cc.winSize.height/2
         });
         this.addChild(background);
 
-        this.sprite = new cc.Sprite("res/cross.png");
-        this.sprite.attr({
-            x: cc.winSize.width / 2,
-            y: cc.winSize.height / 2
+        var red_circle = new cc.Sprite("res/circle.png"); // красный круг
+        red_circle.attr({
+            x: cc.winSize.width / 2 - 100,
+            y: cc.winSize.height / 2 + 270
         });
-        this.addChild(this.sprite);
-        this.sprites.push(this.sprite);
+        this.addChild(red_circle);
+        this.sprites.push(red_circle);
 
-        var newSprite2 = new cc.Sprite("res/cross.png");
-        newSprite2.attr({
+        var red_cross = new cc.Sprite("res/cross.png"); // красный крест
+        red_cross.attr({
+            x: cc.winSize.width / 2 - 0,
+            y: cc.winSize.height / 2 + 270
+        });
+        this.addChild(red_cross);
+        this.sprites.push(red_cross);
+
+        var red_triangle = new cc.Sprite("res/triangle.png"); // красный треугольник
+        red_triangle.attr({
             x: cc.winSize.width / 2 + 100,
-            y: cc.winSize.height / 2
+            y: cc.winSize.height / 2 + 270
         });
-        this.addChild(newSprite2);
-        this.sprites.push(newSprite2);
+        this.addChild(red_triangle);
+        this.sprites.push(red_triangle);
 
-        for (var i = 0; i < 3; i++) {
+
+        var blue_circle = new cc.Sprite("res/blue_circle.png");// синий круг
+        blue_circle.attr({
+            x: cc.winSize.width / 2 - 100,
+            y: cc.winSize.height / 2 - 270
+        });
+        this.addChild(blue_circle);
+        this.sprites.push(blue_circle);
+
+        var blue_cross = new cc.Sprite("res/blue_cross.png"); // синий крест
+        blue_cross.attr({
+            x: cc.winSize.width / 2 + 0,
+            y: cc.winSize.height / 2 - 270
+        });
+        this.addChild(blue_cross);
+        this.sprites.push(blue_cross);
+
+        var blue_traingle = new cc.Sprite("res/blue_triangle.png"); // синий треугольник
+        blue_traingle.attr({
+            x: cc.winSize.width / 2 + 100,
+            y: cc.winSize.height / 2 - 270
+        });
+        this.addChild(blue_traingle);
+        this.sprites.push(blue_traingle);
+        
+
+        for (var i = 0; i < 3; i++) { // размтека сетки
             for (var j = 0; j < 3; j++) {
                 var cell = new cc.Sprite("res/cell.png");
                 cell.attr({
-                    x: cc.winSize.width / 4 + (i * cc.winSize.width / 4),
-                    y: cc.winSize.height / 4 + (j * cc.winSize.height / 4)
+                    x: cc.winSize.width / 3.5 + (i * cc.winSize.width / 5),
+                    y: cc.winSize.height / 3.5 + (j * cc.winSize.height / 5)
                 });
                 this.addChild(cell);
                 this.grid.push(cell);
             }
         }
 
-        cc.eventManager.addListener({
+        cc.eventManager.addListener({ // обработчик драго эн дроп
             event: cc.EventListener.MOUSE,
             onMouseDown: this.processMouseDown.bind(this),
             onMouseMove: this.processMouseMove.bind(this),
@@ -62,7 +96,7 @@ var HelloWorldLayer = cc.Layer.extend({
         return true;
     },
 
-    processMouseDown:function(event){
+    processMouseDown:function(event){ // мышь вниз
         var location = event.getLocation();
         for (var i = 0; i < this.sprites.length; i++) {
             if (cc.rectContainsPoint(this.sprites[i].getBoundingBox(), location) && !this.sprites[i].isLocked) {
@@ -73,7 +107,7 @@ var HelloWorldLayer = cc.Layer.extend({
         }
     },
 
-    processMouseMove:function(event){
+    processMouseMove:function(event){ // движение по горизонтали
         var location = event.getLocation();
         if(this.isDragging){
             this.sprite.x = location.x;
@@ -81,14 +115,14 @@ var HelloWorldLayer = cc.Layer.extend({
         }
     },
 
-    processMouseUp:function(event){
+    processMouseUp:function(event){ // движение вверх
         this.isDragging = false;
         if(this.isInTarget()){
             this.sprite.isLocked = true;
         }
     },
 
-    isInTarget:function(){
+    isInTarget:function(){ // в клетке или нет
         for (var i = 0; i < this.grid.length; i++) {
             if (cc.rectContainsPoint(this.grid[i].getBoundingBox(), cc.p(this.sprite.x, this.sprite.y))) {
                 return true;
